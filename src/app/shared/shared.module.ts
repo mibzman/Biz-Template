16718337a151a8290971113c49c340542dbaf7c4
mibzman <sam@hungerperks.com>
2018-05-30 11:27:59 -0400

    moved material module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { MaterialModule } from "../material.module";
+import { MaterialModule } from "./material.module";
 import { ServicesModule } from '../services/services.module'
 
 const MODULES = [CommonModule, MaterialModule, ServicesModule];
